08c079ef3e9f9c79b77b6bf751a4eb9c19b02fdf
juan <jj1913@nyu.edu>
2018-02-18 16:55:39 -0500

    setting up tests

--- apps/forum/tests.py
+++ apps/forum/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Board, Thread, Post
 
 # Create your tests here.
+
+class BoardTestCase(TestCase):
+    """
+    Tests the ORM functionality for the Board model
+    """
+    def setUp(self):
+        """
+        set up for the tests
+        """
+
+class ThreadTestCase(TestCase):
+    """
+    Tests the ORM functionality for the Thread model
+    """
+    
+    def setUp(self):
+        """
+        set up for the tests
+        """
+
+
+class PostTestCase(TestCase):
+    """
+    Tests the OR functionality for the Post model
+    """
+
+    def setUp(self):
+        """
+        set up for the tests
+        """
