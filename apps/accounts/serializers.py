c9ea639f0087de139de387013d7f0a46f3a88572
Juan <jj1913@nyu.edu>
2018-03-15 22:32:01 -0400

    can register now

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -9,8 +9,33 @@ from rest_framework.serializers import (
     ReadOnlyField,
     Field
     )
-
+from rest_framework.validators import UniqueValidator
+from rest_framework import serializers
+from django.contrib.auth.models import User
 from apps.accounts.models import Profile, Clan, ProfileImg
+
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+    username = serializers.CharField(
+            max_length=32,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
+
+    password = serializers.CharField(min_length=8)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'],
+             validated_data['password'])
+        return user
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+
 
 class ProfileCreateUpdateSerializer(ModelSerializer):
     class Meta:
@@ -26,7 +51,8 @@ class ProfileDetailSerializer(ModelSerializer):
     name = ReadOnlyField()
     class Meta:
         model = Profile
-        fields = [ 
+        fields = [
+            'id',
             'name',
             'clan',
             'image',
