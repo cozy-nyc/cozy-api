982aea3e011d9996bb5b6d25e28aa006cdfe1fc2
rantahu <rantahu@gmail.com>
2018-06-09 17:16:21 -0400

    removed settings.py and fixed base_dir location

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ environ.Env.read_env(env_file=base('.env'))
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
