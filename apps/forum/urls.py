9d1021617cd8e494bbd7d907b963309be09cc30b
Juan <jj1913@nyu.edu>
2018-09-23 11:53:28 -0400

    changes to board

--- apps/forum/urls.py
+++ apps/forum/urls.py
@@ -5,7 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^board/$', views.BoardList.as_view()),
-    url(r'^board/(?P<pk>[0-9]+)/$', views.BoardDetail.as_view()),
+    url(r'^board/(?P<tag>[\w\-]+)/$', views.BoardDetail.as_view()),
     url(r'^board/create$', views.BoardCreate.as_view()),
     url(r'^thread/$', views.ThreadList.as_view()),
     url(r'^thread/create$', views.ThreadCreate.as_view()),
