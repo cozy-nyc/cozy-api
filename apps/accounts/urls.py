6f1744862f411e646df5072671249d2c9e18da1a
S1MB10T3 <s1mb10t3@pm.me>
2019-08-09 19:26:22 -0400

    removed dead urls

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url, include
 from . import views
 from django.views.generic import TemplateView
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 from django.contrib.auth import views as auth_views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -17,9 +16,6 @@ urlpatterns = [
     url(r'^api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     url(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
     url(r'^api/token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-refresh/', refresh_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
     url(r'^profile/$', views.ProfileList.as_view()),
     url(r'^profile/(?P<user__username>[\w\-]+)/$', views.ProfileDetail.as_view()),
     url(r'^profile/edit/(?P<pk>[0-9]+)/$', views.ProfileUpdate.as_view()),
