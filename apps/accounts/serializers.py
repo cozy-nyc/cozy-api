71b4df530f5b8ed531e4b4a6435ace744ebb0c43
S1MB10T3 <s1mb10t3@pm.me>
2019-08-09 22:24:23 -0400

    auth token now returns username

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -15,7 +15,19 @@ from django.contrib.auth.models import User
 from apps.accounts.models import Profile
 from django.contrib.auth.models import User
 from rest_auth.models import TokenModel
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
 
 
 class ProfileCreateUpdateSerializer(ModelSerializer):
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -13,9 +13,9 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    url(r'^api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
-    url(r'^api/token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
+    url(r'^auth/token/$', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^auth/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^auth/token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
     url(r'^profile/$', views.ProfileList.as_view()),
     url(r'^profile/(?P<user__username>[\w\-]+)/$', views.ProfileDetail.as_view()),
     url(r'^profile/edit/(?P<pk>[0-9]+)/$', views.ProfileUpdate.as_view()),
